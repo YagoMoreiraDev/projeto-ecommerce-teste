582aa12a50b3ecb823c77e15ece21891473bd7d0
Yago Moreira <yagomoreira_dev@yahoo.com>
2024-05-04 21:04:57 -0300

    Iniciando Projeto ecommerce-test, já criado entidades User e Order e os devidos relacionamentos

---
+++ backend/src/main/java/com/cloud/yagodev/backend/domain/entities/Order.java
@@ -0,0 +1,57 @@
+package com.cloud.yagodev.backend.domain.entities;
+
+import com.cloud.yagodev.backend.domain.enums.OrderStatus;
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private Instant moment;
+    private OrderStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+    public Order() {
+    }
+
+    public Order(UUID id, Instant moment, OrderStatus status) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+}
---
+++ backend/src/main/java/com/cloud/yagodev/backend/domain/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.cloud.yagodev.backend.domain.enums;
+
+public enum OrderStatus {
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED;
+}
