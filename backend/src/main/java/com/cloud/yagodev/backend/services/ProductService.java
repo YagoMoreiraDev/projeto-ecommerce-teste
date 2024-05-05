609b7fc7938cfc43673dc2bca403a77a02ffb055
Yago Moreira <yagomoreira_dev@yahoo.com>
2024-05-05 20:51:08 -0300

    GET findAll - Pageable

--- backend/src/main/java/com/cloud/yagodev/backend/services/ProductService.java
+++ backend/src/main/java/com/cloud/yagodev/backend/services/ProductService.java
@@ -3,9 +3,12 @@ package com.cloud.yagodev.backend.services;
 import com.cloud.yagodev.backend.domain.dtos.ProductDTO;
 import com.cloud.yagodev.backend.domain.entities.Product;
 import com.cloud.yagodev.backend.repositories.ProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,5 +24,11 @@ public class ProductService {
     public ProductDTO findById(Long id) {
         Product product = productRepository.findById(id).get();
         return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = productRepository.findAll(pageable);
+        return result.map(ProductDTO::new);
     }
 }
